aefe39b8b6816504cfcb977428b99e971e457787
B-Deprez <bruno.r.deprez@gmail.com>
2021-06-25 16:41:52 +0200

    Deleted GAM for now. Takes to long to fit.

--- CANN_Reproduction.R
+++ CANN_Reproduction.R
@@ -19,12 +19,4 @@ model_1 <- glm(nclaims ~ coverage + ageph + sex + bm + power + agec +
                offset = log(expo))
 summary(model_1)
 
-#GAM
-model_2 <- gam(nclaims ~ coverage + s(ageph) + sex + s(bm) + s(power) + s(agec) +
-                 fuel + use + fleet + postcode, 
-               family = poisson(), 
-               data = mtpl_be, 
-               offset = log(expo))
-summary(model_2)
 
-#GBM
