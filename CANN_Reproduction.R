30e9120f99d723cfce754268e684401a2e828b11
B-Deprez <bruno.r.deprez@gmail.com>
2021-06-27 15:55:08 +0200

    Finalise network with embedding.

--- CANN_Reproduction.R
+++ CANN_Reproduction.R
@@ -53,7 +53,7 @@ model_2 %>% compile(optimizer = optimizer_nadam(),
                     loss = "poisson")
 
 Xfeat <- as.matrix(mtpl_be_onehot)
-Xexpo <- as.matrix(mtpl_be$expo)
+Xexpo <- as.matrix(log(mtpl_be$expo))
 Ylearn <- as.matrix(mtpl_be$nclaims)
 
 fit_2 <- model_2 %>% fit(list(Xfeat, Xexpo), Ylearn, 
@@ -67,20 +67,25 @@ non_cat <- c(5, 7:9)
 n0 <- length(non_cat)
 
 Xlearn <- as.matrix(mtpl_be[,non_cat])
-CovLearn <- as.matrix(as.integer(mtpl_be$coverage))
+Vlearn <- as.matrix(log(mtpl_be$expo))
+CovLearn <- as.matrix(as.integer(mtpl_be$coverage))-1
 NCov <- length(unique(CovLearn))
-SexLearn <- as.matrix(as.integer(mtpl_be$sex))
+SexLearn <- as.matrix(as.integer(mtpl_be$sex))-1
 NSex <- length(unique(SexLearn))
-FuelLearn <- as.matrix(as.integer(mtpl_be$fuel))
+FuelLearn <- as.matrix(as.integer(mtpl_be$fuel))-1
 NFuel <- length(unique(FuelLearn))
-UseLearn <- as.matrix(as.integer(mtpl_be$use))
+UseLearn <- as.matrix(as.integer(mtpl_be$use))-1
 NUse <- length(unique(UseLearn))
-FleetLearn <- as.matrix(as.integer(mtpl_be$fleet))
+FleetLearn <- as.matrix(as.integer(mtpl_be$fleet))-1
 NFleet <- length(unique(FleetLearn))
-PcLearn <- as.matrix(as.integer(mtpl_be$postcode))
+PcLearn <- as.matrix(as.integer(mtpl_be$postcode))-1
 NPc <- length(unique(PcLearn))
 
+Ylearn <- as.matrix(mtpl_be$nclaims)
+
 Design <- layer_input(shape = c(n0), dtype = 'float32', name = 'Design')
+
+LogVol <- layer_input(shape = c(1),   dtype = 'float32', name = 'LogVol')
 
 Coverage <- layer_input(shape = c(1), dtype = 'int32', name = 'Coverage')
 Sex <- layer_input(shape = c(1), dtype = 'int32', name = 'Sex')
@@ -90,26 +95,53 @@ Fleet <- layer_input(shape = c(1), dtype = 'int32', name = 'Fleet')
 PostalCode <- layer_input(shape = c(1), dtype = 'int32', name = 'PostalCode')
 
 CovEmb = Coverage %>%
-  layer_embedding(input_dim = NCov, output_dim = 2, input_length = 1, name = "CovEmb") %>%
+  layer_embedding(input_dim = NCov, output_dim = 1, input_length = 1, name = "CovEmb") %>%
   layer_flatten(name = "Cov_flat") 
 
 SexEmb = Sex %>%
-  layer_embedding(input_dim = NSex, output_dim = 2, input_length = 1, name = "SexEmb") %>%
+  layer_embedding(input_dim = NSex, output_dim = 1, input_length = 1, name = "SexEmb") %>%
   layer_flatten(name = "Sex_flat") 
 
 FuelEmb = Fuel %>%
-  layer_embedding(input_dim = NFuel, output_dim = 2, input_length = 1, name = "FuelEmb") %>%
+  layer_embedding(input_dim = NFuel, output_dim = 1, input_length = 1, name = "FuelEmb") %>%
   layer_flatten(name = "Fuel_flat") 
 
 UsageEmb = Usage %>%
-  layer_embedding(input_dim = NUse, output_dim = 2, input_length = 1, name = "UsageEmb") %>%
+  layer_embedding(input_dim = NUse, output_dim = 1, input_length = 1, name = "UsageEmb") %>%
   layer_flatten(name = "Usage_flat") 
 
 FleetEmb = Fleet %>%
-  layer_embedding(input_dim = NFleet, output_dim = 2, input_length = 1, name = "FleetEmb") %>%
+  layer_embedding(input_dim = NFleet, output_dim = 1, input_length = 1, name = "FleetEmb") %>%
   layer_flatten(name = "Fleet_flat") 
 
 PcEmb = PostalCode %>%
-  layer_embedding(input_dim = NPc, output_dim = 2, input_length = 1, name = "PcEmb") %>%
+  layer_embedding(input_dim = NPc, output_dim = 3, input_length = 1, name = "PcEmb") %>%
   layer_flatten(name = "Pc_flat") 
 
+q1 <- 30   
+q2 <- 15
+q3 <- 10
+
+Network <- list(Design, CovEmb, SexEmb, FuelEmb, UsageEmb, FleetEmb, PcEmb) %>%
+  layer_concatenate(name = 'concate') %>%
+  layer_dense(units=q1, activation='tanh', name='hidden1') %>%
+  layer_dense(units=q1, activation='tanh', name='hidden2') %>%
+  layer_dense(units=q2, activation='tanh', name='hidden3') %>%
+  layer_dense(units=q3, activation='tanh', name='hidden4') %>%
+  layer_dense(units=1, activation='linear', name='Network')
+
+Response <- list(Network, LogVol) %>% layer_add(name='Add') %>% 
+  layer_dense(units=1, activation=k_exp, name = 'Response', trainable=FALSE,
+              weights=list(array(1, dim=c(1,1)), array(0, dim=c(1))))
+
+model_3 <- keras_model(inputs = c(Design, Coverage, Sex, Fuel, Usage, Fleet, PostalCode, LogVol),
+                       outputs = c(Response))
+
+model_3 %>% compile(optimizer = optimizer_nadam(), loss = 'poisson')
+
+fit_3 <- model_3 %>% fit(list(Xlearn, CovLearn, SexLearn, FuelLearn, UseLearn, FleetLearn, PcLearn, Vlearn),
+                         Ylearn, 
+                         epochs = 20, 
+                         batch_size = 1718,
+                         validation_split = 0.2)
+fit_3$metrics$loss[10]
