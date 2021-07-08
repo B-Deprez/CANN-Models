9690e8290c47ddc2aa9a33d089510def2c76764e
B-Deprez <bruno.r.deprez@gmail.com>
2021-07-08 18:40:33 +0200

    Added CANN to comparison

--- CANN_MTPL.R
+++ CANN_MTPL.R
@@ -22,12 +22,14 @@ belgium_shape_sf <- st_read('./shape file Belgie postcodes/npc96_region_Project1
 belgium_shape_sf <- st_transform(belgium_shape_sf, CRS("+proj=longlat +datum=WGS84"))
 
 #### Fist small analysis why CANN, and not just NN ####
-# Age
+### Age
+#GLM
 GAM_age <- gam(nclaims ~ s(ageph) + offset(log(expo)), 
                data = mtpl_be, 
                family = poisson(link = "log"))
 plot(GAM_age)
 
+#NN
 Design <- layer_input(shape = c(1), dtype = "float32", name = "Design")
 LogExpo <- layer_input(shape = c(1), dtype = "float32", name = "LogExpo")
 
@@ -57,8 +59,35 @@ fit_nn_age <- NN_age %>% fit(list(Xfeat, Xexpo), Ylearn,
                          validation_split = 0.2)
 
 fit_nn_age$metrics$loss[10]
+#CANN
+Vlearn <- as.matrix(log(fitted(GAM_age)))
+LogGAM <- layer_input(shape = c(1),   dtype = 'float32', name = 'LogGLM')
 
-#Compare GAM and NN
+Design2 <- layer_input(shape = c(1), dtype = "float32", name = "Design2")
+
+Network2 <- Design2 %>%
+  layer_batch_normalization(input_shape = c(1)) %>%
+  layer_dense(units = 5, activation = 'tanh', name = 'hidden1') %>%
+  layer_dense(units = 5, activation = 'tanh', name = 'hidden2') %>%
+  layer_dense(units = 1, activation = 'linear', name = 'Network2')
+
+Response2 <- list(Network2, LogGAM) %>%
+  layer_add(name= "Add") %>%
+  layer_dense(units=1, activation=k_exp, name = 'Response2', trainable=FALSE,
+              weights=list(array(1, dim=c(1,1)), array(0, dim=c(1))))
+
+CANN_age <- keras_model(inputs = c(Design2, LogGAM), 
+                        outputs = c(Response2))
+
+CANN_age %>% compile(optimizer = optimizer_adam(), 
+                     loss = "poisson")
+
+fit_CANN_age <- CANN_age%>% fit(list(Xfeat, Vlearn), Ylearn, 
+                                epochs = 10, 
+                                batch_size = 1718,
+                                validation_split = 0.2)
+
+#Compare GAM and NN+CANN
 XTest <- min(mtpl_be$ageph):max(mtpl_be$ageph)
 ExpoTest <- rep(1, length(XTest))
 
@@ -67,11 +96,14 @@ y_fit_NN <- predict(NN_age, list(XTest, log(ExpoTest)))
 y_fit_GAM <- predict(GAM_age, newdata = data.frame(ageph = XTest, expo = 1), 
                      type = "response")
 
-Comparison <- tibble(Age = XTest, GAM = y_fit_GAM, NN = y_fit_NN)
+y_fit_CANN <- predict(CANN_age, list(XTest, log(y_fit_GAM)))
+
+Comparison <- tibble(Age = XTest, GAM = y_fit_GAM, NN = y_fit_NN, CANN = y_fit_CANN)
 
 ggplot(Comparison, aes(x = Age)) +
   geom_line(aes(y= GAM), color = "blue") +
   geom_line(aes(y= NN), color = "red") +
+  geom_line(aes(y= CANN), color = "darkgreen")+
   ylab("Response") +
   ggtitle("Difference between GAM and Neural Network")+
   theme_bw()
