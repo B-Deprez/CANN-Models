f9d3284ab84dd6b0f0f5e6f4d21448bc73e8edd3
B-Deprez <bruno.r.deprez@gmail.com>
2021-07-08 16:58:12 +0200

    Added the NN for age

--- CANN_MTPL.R
+++ CANN_MTPL.R
@@ -28,4 +28,48 @@ GAM_age <- gam(nclaims ~ s(ageph) + offset(log(expo)),
                family = poisson(link = "log"))
 plot(GAM_age)
 
-# For the NN we use an embedding layer
+Design <- layer_input(shape = c(1), dtype = "float32", name = "Design")
+LogExpo <- layer_input(shape = c(1), dtype = "float32", name = "LogExpo")
+
+Network <- Design %>%
+  layer_batch_normalization() %>%
+  layer_dense(units = 5, activation = 'tanh', name = 'hidden1') %>%
+  layer_dense(units = 5, activation = 'tanh', name = 'hidden2') %>%
+  layer_dense(units = 1, activation = 'linear', name = 'Network')
+
+Response <- list(Network, LogExpo) %>%
+  layer_add(name = 'Add') %>%
+  layer_dense(units = 1, activation = k_exp, name = 'Response', trainable = FALSE,
+              weights = list(array(1, dim=c(1,1)), array(0, dim = c(1))))
+
+NN_age <- keras_model(inputs = c(Design, LogExpo), outputs = c(Response))
+
+NN_age %>% compile(optimizer = optimizer_adam(), 
+                    loss = "poisson")
+
+Xfeat <- as.matrix(mtpl_be$ageph)
+Xexpo <- as.matrix(log(mtpl_be$expo))
+Ylearn <- as.matrix(mtpl_be$nclaims)
+
+fit_nn_age <- NN_age %>% fit(list(Xfeat, Xexpo), Ylearn, 
+                         epochs = 10, 
+                         batch_size = 1718,
+                         validation_split = 0.2)
+
+fit_nn_age$metrics$loss[10]
+
+#Compare GAM and NN
+
+XTest <- min(mtpl_be$ageph):max(mtpl_be$ageph)
+ExpoTest <- rep(1, length(XTest))
+y_fit_NN <- NN_age %>% predict(list(XTest, ExpoTest))
+
+y_fit_GAM <- predict(GAM_age, newdata = data.frame(ageph = XTest, expo = 1), 
+                     type = "response", se.fit = TRUE)
+
+Comparison <- tibble(Age = XTest, GAM = y_fit_GAM$fit, NN = y_fit_NN)
+
+ggplot(Comparison, aes(x = Age)) +
+  geom_line(aes(y= GAM), color = "blue") +
+  geom_line(aes(y= NN), color = "red") +
+  theme_bw()
