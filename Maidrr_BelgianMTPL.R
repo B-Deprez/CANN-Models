9827f377904e146d44348ad67bcfba6605eae667
B-Deprez <bruno.r.deprez@gmail.com>
2021-09-12 10:14:09 +0200

    Starting implementation of Maidrr example + adding saved GAM model

---
+++ Maidrr_BelgianMTPL.R
@@ -0,0 +1,17 @@
+# Libraries needed
+library(tidyverse)
+library(readxl)
+library(MASS)
+library(readxl)
+library(maidrr)
+library(sp)
+library(sf)
+library(tmap)
+library(mapview)
+library(mgcv)
+
+library(keras)
+
+model <- load_model_tf("CANN_3Layers")
+
+
